53b3868eef7969c6eee8b0eb1253a63453b93a97
Zoltan Szucs <ironhard93@gmail.com>
2017-08-07 08:52:06 +0200

    some hello world imports

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -1,7 +1,8 @@
 package HelloWorld;
+import static java.lang.System.out;
 
 public class HelloWorld {
     public static void main(String args[]){
-        System.out.println("Hello World!");
+        out.println("Hello World!");
     }
 }
